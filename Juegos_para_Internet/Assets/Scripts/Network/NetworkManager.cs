f5966ccb783bb5ca1b6f576adc96b7daa96b7a8d
FerSO-PC\Fer <fer.ochoa.ar@gmail.com>
2016-11-10 16:57:32 -0300

    no message

---
+++ Juegos_para_Internet/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string VERSION = "V0.0.1";
+	public string roomName = "OurRoom";
+	public string playerPrefabName = "Car";
+	public Transform spawnPoint;
+
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings (VERSION);
+		PhotonNetwork.autoJoinLobby = true;
+	
+	}
+	
+	void OnJoinedLobby()
+	{
+		RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+		PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+	}
+
+	void OnJoinedRoom()
+	{
+		PhotonNetwork.Instantiate (playerPrefabName,spawnPoint.position, spawnPoint.rotation,0);
+	}
+}
---
+++ Juegos_para_Internet/Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NetworkPlayer : Photon.MonoBehaviour{
+
+	public GameObject myCamera;
+
+
+	void Start () {
+		
+		if (photonView.isMine) 
+		{
+			myCamera.SetActive (true);
+			GetComponent<CarController> ().enabled = true;
+			GetComponent<CarUserControl> ().enabled = true;
+			GetComponent<CarAudio> ().enabled = true;
+		   
+		}
+	}
+	
+
+}
