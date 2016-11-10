9e06cffc2610dc14b5057bcbe6ddd6e7d060e4ed
FerSO-PC\Fer <fer.ochoa.ar@gmail.com>
2016-11-10 19:28:31 -0300

    no message

--- Juegos_para_Internet/Assets/Scripts/Network/NetworkPlayer.cs
+++ Juegos_para_Internet/Assets/Scripts/Network/NetworkPlayer.cs
@@ -5,18 +5,61 @@ using System.Collections;
 public class NetworkPlayer : Photon.MonoBehaviour{
 
 	public GameObject myCamera;
+	private bool isAlive = true;
+	private Vector3 position;
+	private Quaternion rotation;
+	private float lerpSmothing = 5f;
 
-
-	void Start () {
+	void Start ()
+	{
 		
-		if (photonView.isMine) 
-		{
+		if (photonView.isMine) {
+
+			gameObject.name = "Me";
+
 			myCamera.SetActive (true);
 			GetComponent<CarController> ().enabled = true;
 			GetComponent<CarUserControl> ().enabled = true;
 			GetComponent<CarAudio> ().enabled = true;
 		   
 		}
+		else 
+		{
+			gameObject.name = "Network Player";
+			StartCoroutine ("Alive");
+		} 
+	}
+
+
+	void OnPhotonSerializeView(PhotonStream stream , PhotonMessageInfo info)
+	{
+		if (stream.isWriting) 
+		{
+			stream.SendNext (transform.position);
+			stream.SendNext (transform.rotation);
+
+		} 
+		else 
+		{
+			position = (Vector3)stream.ReceiveNext ();
+			rotation = (Quaternion)stream.ReceiveNext ();
+		}
+
+	}
+
+
+	IEnumerator Alive()
+	{
+		while (isAlive) 
+		{
+
+			transform.position = Vector3.Lerp (transform.position, position, Time.deltaTime * lerpSmothing);
+			transform.rotation = Quaternion.Lerp (transform.rotation, rotation, Time.deltaTime * lerpSmothing);
+
+			yield return null;
+		}
+
+
 	}
 	
 
