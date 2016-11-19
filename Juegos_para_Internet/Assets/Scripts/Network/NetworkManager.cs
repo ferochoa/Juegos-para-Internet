c279ae131923a312753d9923c4fa0a0dd5e4b682
FerSO-PC\Fer <fer.ochoa.ar@gmail.com>
2016-11-19 00:27:03 -0300

    no message

--- Juegos_para_Internet/Assets/Scripts/Network/NetworkManager.cs
+++ Juegos_para_Internet/Assets/Scripts/Network/NetworkManager.cs
@@ -1,27 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviour {
 
 	private const string VERSION = "V0.0.1";
-	public string roomName = "OurRoom";
 	public string playerPrefabName = "Car";
-	public Transform spawnPoint;
+	
+    private RoomInfo[] roomsList;
+    public GameObject menu;
+    public GameObject createButton;
+    public GameObject joinButton;
+    public GameObject lobbyPanel;
+    public GameObject [] spawPoints;
+    private int playersNumber ;
+    public Text text; 
 
-	void Start () {
+    void Start () {
 		PhotonNetwork.ConnectUsingSettings (VERSION);
-		PhotonNetwork.autoJoinLobby = true;
+		//PhotonNetwork.autoJoinLobby = true;
 	
 	}
-	
-	void OnJoinedLobby()
-	{
-		RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
-		PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
-	}
+    void OnGUI()
+    {
+
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else 
+        {
+            GUILayout.Label("Connection Ready");
+            lobbyPanel.gameObject.SetActive(true);
+        }
+
+        
+
+    }
+
+   
+
+    void OnJoinedLobby()
+    {
+        if (PhotonNetwork.room == null)
+        {
+            createButton.SetActive(true);
+
+        }
+       
+        
+           
+            joinButton.gameObject.SetActive(true);
+        
+        
+
+
+    }
 
 	void OnJoinedRoom()
 	{
-		PhotonNetwork.Instantiate (playerPrefabName,spawnPoint.position, spawnPoint.rotation,0);
-	}
+       playersNumber = PhotonNetwork.countOfPlayersInRooms;
+        menu.gameObject.SetActive(false);      
+        PhotonNetwork.Instantiate(playerPrefabName, spawPoints[playersNumber].transform.position, spawPoints[playersNumber].transform.rotation, 0);
+         
+       
+    }
+
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+       
+    }
+
+    public void createRoom()
+    {
+        PhotonNetwork.CreateRoom("My room", new RoomOptions() { maxPlayers = 4 }, TypedLobby.Default);
+        
+    }
+
+
+   public void joinRoom()
+    {
+        if (roomsList.Length == 0)
+        {
+            text.gameObject.SetActive(true);
+            text.text = "No hay rooms disponibles";
+        }
+        else
+        {
+            for (int i = 0; i < roomsList.Length; i++)
+            {
+
+                PhotonNetwork.JoinRoom(roomsList[i].name);
+
+            }
+        }
+    }
+   
 }
